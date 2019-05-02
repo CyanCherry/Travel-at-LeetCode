489e6c9ba442b830cb250335a61cc24eb72b5fdd
Cyan Cherry <cyan_cherry@qq.com>
2019-05-02 17:54:12 +0800

    Finished moveZeroes.

---
+++ PrimaryAlgorithm/A_Array/H_moveZeroes.java
@@ -0,0 +1,27 @@
+package A_Array;
+
+import java.util.Arrays;
+
+public class H_moveZeroes {
+    static class Solution {
+        static void moveZeroes(int[] nums) {
+            int notZeroNumber = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    if (i != notZeroNumber) {
+                        int tmp = nums[i];
+                        nums[i] = nums[notZeroNumber];
+                        nums[notZeroNumber] = tmp;
+                    }
+                    notZeroNumber++;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{0, 1, 0, 3, 12};
+        Solution.moveZeroes(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
