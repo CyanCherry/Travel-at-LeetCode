6b3f83d05fb9e34271d0193ba698bdf52326c8b7
CyanChery <cyan_cherry@qq.com>
2019-06-13 11:29:02 +0800

    Finished array, the story of string unfolds.

---
+++ PrimaryAlgorithm/B_String/A_reverseString.java
@@ -0,0 +1,27 @@
+package B_String;
+
+public class A_reverseString {
+    static class Solution {
+        static void reverseString(char[] s) {
+            int maxIndex = s.length - 1;
+            int endIndex = s.length / 2;
+            char tmp;
+            int anotherIndex;
+            for (int index = 0; index < endIndex; index++) {
+                tmp = s[index];
+                anotherIndex = maxIndex - index;
+                s[index] = s[anotherIndex];
+                s[anotherIndex] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] test1 = new char[]{'h', 'e', 'l', 'l', 'o'};
+        char[] test2 = new char[]{'H', 'a', 'n', 'n', 'a', 'h'};
+        Solution.reverseString(test1);
+        Solution.reverseString(test2);
+        System.out.println(test1);
+        System.out.println(test2);
+    }
+}
