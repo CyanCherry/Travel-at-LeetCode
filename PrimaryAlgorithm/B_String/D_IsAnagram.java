0afc28d5e21f4d035025650304b2130d9c4cd4d8
Cyan Cherry <cyan_cherry@qq.com>
2019-08-24 23:57:41 +0800

    Replace charAt with toCharArray and access the array with index is a good way to make Java program faster.

---
+++ PrimaryAlgorithm/B_String/D_IsAnagram.java
@@ -0,0 +1,28 @@
+package B_String;
+
+public class D_IsAnagram {
+    static class Solution {
+        static boolean isAnagram(String s, String t) {
+            if (s.length() != t.length())
+                return false;
+            int[] statistic = new int[26];
+            int length = s.length();
+            char[] sArray = s.toCharArray();
+            char[] tArray = t.toCharArray();
+            for (int index = 0; index < length; index++) {
+                statistic[sArray[index] - 'a']++;
+                statistic[tArray[index] - 'a']--;
+            }
+            for (int element : statistic)
+                if (element != 0)
+                    return false;
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.isAnagram("", ""));
+        System.out.println(Solution.isAnagram("cyan", "ycan"));
+        System.out.println(Solution.isAnagram("cherry", "cehryr"));
+    }
+}
