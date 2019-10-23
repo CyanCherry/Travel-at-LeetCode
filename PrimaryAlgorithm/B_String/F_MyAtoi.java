eea6912a2ad9706f4187eb9e81e81097412bb08e
Cyan Cherry <cyan_cherry@qq.com>
2019-10-23 10:54:31 +0800

    Finished MyAtoi.

---
+++ PrimaryAlgorithm/B_String/F_MyAtoi.java
@@ -0,0 +1,50 @@
+package B_String;
+
+public class F_MyAtoi {
+    static class Solution {
+        static int myAtoi(String str) {
+            char[] strArray = str.toCharArray();
+            int index = 0;
+            int result;
+            boolean isPositive = true;
+            boolean isNumber = false;
+            while (index < strArray.length && strArray[index] == ' ')
+                index++;
+            StringBuilder resultBuilder = new StringBuilder();
+            if (index < strArray.length && (strArray[index] == '-' || strArray[index] == '+')) {
+                isPositive = strArray[index] == '+';
+                resultBuilder.append(strArray[index]);
+                index++;
+            }
+            while (index < strArray.length && strArray[index] > '/' && strArray[index] < ':') {
+                resultBuilder.append(strArray[index]);
+                isNumber = true;
+                index++;
+            }
+            String resultStr = resultBuilder.toString();
+            try {
+                result = Integer.parseInt(resultStr);
+            } catch (NumberFormatException exception) {
+                return isNumber ? isPositive ? Integer.MAX_VALUE : Integer.MIN_VALUE : 0;
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.myAtoi("   -555"));
+        System.out.println(Solution.myAtoi("    555"));
+        System.out.println(Solution.myAtoi("555"));
+        System.out.println(Solution.myAtoi("-555"));
+        System.out.println(Solution.myAtoi("-555555555555555555555"));
+        System.out.println(Solution.myAtoi("5555555555555555555555"));
+        System.out.println(Solution.myAtoi("cherry"));
+        System.out.println(Solution.myAtoi("     "));
+        System.out.println(Solution.myAtoi("  -"));
+        System.out.println(Solution.myAtoi("  +1"));
+        System.out.println(Solution.myAtoi(String.valueOf(Integer.MAX_VALUE)));
+        System.out.println(Solution.myAtoi(String.valueOf(Integer.MIN_VALUE)));
+        System.out.println(Solution.myAtoi("2147483648"));
+        System.out.println(Solution.myAtoi("-2147483649"));
+    }
+}
