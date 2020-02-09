66c5eb367189bac3a2a095e4fc730df1be2a98c7
Cyan Cherry <cyan_cherry@qq.com>
2020-02-09 12:41:32 +0800

    Yang Hui triangle is also known as the Pascal triangle.

---
+++ PrimaryAlgorithm/I_Others/D_YangHuiTriangle.java
@@ -0,0 +1,31 @@
+package I_Others;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class D_YangHuiTriangle {
+    static class Solution {
+        static List<List<Integer>> generate(int numRows) {
+            System.out.println("numRows = " + numRows);
+            Integer[][] result = new Integer[numRows][];
+            for (int i = 0; i < numRows; i++) {
+                Integer[] row = new Integer[i + 1];
+                row[0] = row[i] = 1;
+                for (int index = 1; index < i; index++)
+                    row[index] = result[i - 1][index - 1] + result[i - 1][index];
+                result[i] = row;
+            }
+            List<List<Integer>> resultList = new LinkedList<>();
+            for (Integer[] row : result)
+                resultList.add(Arrays.asList(row));
+            return resultList;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.generate(0));
+        System.out.println(Solution.generate(1));
+        System.out.println(Solution.generate(5));
+    }
+}
