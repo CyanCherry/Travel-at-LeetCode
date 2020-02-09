a706db7c6ce0497c521d3e881b31465e6d709963
Cyan Cherry <cyan_cherry@qq.com>
2020-02-09 11:56:14 +0800

    Simplify logic.

--- PrimaryAlgorithm/I_Others/C_ReverseBits.java
+++ PrimaryAlgorithm/I_Others/C_ReverseBits.java
@@ -6,10 +6,9 @@ public class C_ReverseBits {
         static int reverseBits(int n) {
             int result = 0;
             for (int i = 0; i < 32; i++) {
-                result = result << 1;
-                if ((n & 1) != 0)
-                    result = result ^ 1;
-                n = n >>> 1;
+                result <<= 1;
+                result |= n & 1;
+                n >>>= 1;
             }
             return result;
         }
