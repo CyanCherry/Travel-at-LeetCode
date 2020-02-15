df3eab990606cb05ca402f3660030d73f29f61ca
Cyan Cherry <cyan_cherry@qq.com>
2020-02-15 18:19:32 +0800

    It seems that collections are not suitable for small data sizes.

---
+++ PrimaryAlgorithm/I_Others/E_AreBracketsValid.java
@@ -0,0 +1,36 @@
+package I_Others;
+
+public class E_AreBracketsValid {
+    static class Solution {
+        static boolean isValid(String s) {
+            if (s.length() % 2 != 0)
+                return false;
+            char[] stack = new char[s.length()];
+            int position = -1;
+            for (Character character : s.toCharArray()) {
+                if (character == '{' || character == '[' || character == '(') {
+                    stack[++position] = character;
+                } else if (character == '}') {
+                    if (position == -1 || stack[position--] != '{')
+                        return false;
+                } else if (character == ']') {
+                    if (position == -1 || stack[position--] != '[')
+                        return false;
+                } else if (character == ')') {
+                    if (position == -1 || stack[position--] != '(')
+                        return false;
+                }
+            }
+            return position == -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        assert Solution.isValid("()");
+        assert Solution.isValid("()[]{}");
+        assert !Solution.isValid("(]");
+        assert !Solution.isValid("([)]");
+        assert Solution.isValid("{[]}");
+        assert !Solution.isValid("}[]}");
+    }
+}
