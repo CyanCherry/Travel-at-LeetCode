55e056ddee539f704ce6b865f537a07b38ed4ba1
Cyan Cherry <cyan_cherry@qq.com>
2020-04-12 00:04:15 +0800

    Expanding from a core can avoid check one substring more than one time.

---
+++ B_IntermediateAlgorithm/A_ArrayAndString/5_LongestPalindrome.go
@@ -0,0 +1,32 @@
+package main
+
+func longestPalindrome(s string) string {
+	if len(s) < 2 {
+		return s
+	}
+	maxLen := 0
+	var result string
+	expand := func(start, end int) {
+		for start > -1 && end < len(s) && s[start] == s[end] {
+			start--
+			end++
+		}
+		currentLen := end - start - 1
+		if currentLen > maxLen {
+			maxLen = currentLen
+			result = s[start+1 : end]
+		}
+	}
+	for centerIndex := range s {
+		expand(centerIndex-1, centerIndex+1)
+		expand(centerIndex, centerIndex+1)
+	}
+	return result
+}
+
+func main() {
+	println(longestPalindrome("babad") == "bab")
+	println(longestPalindrome("cbbd") == "bb")
+	println(longestPalindrome("a") == "a")
+	println(longestPalindrome("aaaaaaaaabc") == "aaaaaaaaa")
+}
