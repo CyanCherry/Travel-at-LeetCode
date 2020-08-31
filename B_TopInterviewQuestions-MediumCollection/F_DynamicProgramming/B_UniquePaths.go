01231d4c36cdf19bcf9595590be657ad7de92644
Cyan Cherry <cyan_cherry@qq.com>
2020-08-31 09:21:17 +0800

    Always the same distance.

---
+++ B_TopInterviewQuestions-MediumCollection/F_DynamicProgramming/B_UniquePaths.go
@@ -0,0 +1,19 @@
+package main
+
+func uniquePaths(m int, n int) int {
+	dp := make([]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[j] = dp[j-1] + dp[j]
+		}
+	}
+	return dp[len(dp)-1]
+}
+
+func main() {
+	println(3 == uniquePaths(3, 2))
+	println(28 == uniquePaths(3, 7))
+}
