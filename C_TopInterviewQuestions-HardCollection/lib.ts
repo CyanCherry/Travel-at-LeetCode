05879449069851daab49f1caad63725da96e9785
Cyan Cherry <cyan_cherry@qq.com>
2022-04-09 14:38:03 +0800

    refactor: replace rust with ts

--- C_TopInterviewQuestions-HardCollection/lib.ts
+++ C_TopInterviewQuestions-HardCollection/lib.ts
@@ -1,7 +1,16 @@
 export namespace MyArrays {
-  export function equals(a1: unknown[], a2: unknown[]) {
-    return a1.length === a2.length &&
-      a1.every((v, i) => v == a2[i])
+  function defaultCmp<T>(a: T, b: T): boolean {
+    return a == b;
+  }
+
+  export function equals<T>(a1: T[], a2: T[], cmp?: (a: T, b: T) => boolean) {
+    return a1.length == a2.length &&
+      a1.every((v, i) => (cmp ?? defaultCmp)(v, a2[i]))
+  }
+
+  export function matrixEquals<T>(m1: T[][], m2: T[][], cmp?: (a: T, b: T) => boolean) {
+    return m1.length == m2.length &&
+      m1.every((a, i) => m2[i].every((v, j) => (cmp ?? defaultCmp)(v, a[j])))
   }
 }
 
