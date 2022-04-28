88e8bc1ca12b3e736b1d6ff1a26010ef68458ad0
Cyan Cherry <cyan_cherry@qq.com>
2022-04-28 11:52:52 +0800

    do as more as I can

---
+++ C_TopInterviewQuestions-HardCollection/C_TreesAndGraphs/F_CourseSchedule.ts
@@ -0,0 +1,33 @@
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+    const prerequisiteSet = new Set(prerequisites)
+    const inCount = new Array(numCourses).fill(0)
+    for (let i = 0; i < prerequisites.length; i++) {
+        if (prerequisites[i].length !== 2) {
+            return false
+        }
+        const [from, to] = prerequisites[i]
+        if (from < numCourses && to < numCourses && from != to) {
+            inCount[to]++
+        } else {
+            return false
+        }
+    }
+    while (prerequisiteSet.size) {
+        let isProgressed = false
+        prerequisiteSet.forEach((p) => {
+            const [from, to] = p
+            if (inCount[from] == 0) {
+                inCount[to]--
+                prerequisiteSet.delete(p)
+                isProgressed = true
+            }
+        })
+        if (!isProgressed) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(true === canFinish(2, [[1, 0]]))
+console.log(false === canFinish(2, [[1, 0], [0, 1]]))
